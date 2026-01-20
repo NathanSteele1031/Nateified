e9349521fe8e9b333bc9d18c962697efdff4ad90
Nathan Steele <nsteele1031@gmail.com>
2026-01-19 22:09:43 -0500

    Added Item struct and made clear functions

---
+++ inventory.c
@@ -0,0 +1,15 @@
+typedef struct {
+    char name[20];
+} Item;
+
+void clearItemName(Item* givenItem){
+    for (int index=0; index<20; index++){
+        givenItem->name[index] = '\0';
+    }
+}
+
+void clearInventory(Item* givenInventory, int inventoryLength){
+    for (int index=0; index<inventoryLength; index++){
+        clearItemName(&givenInventory[index]);
+    }
+}
