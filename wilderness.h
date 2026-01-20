ae404cb2d284135eea2233851c24912ebe25822b
Nathan Steele <nsteele1031@gmail.com>
2026-01-20 08:21:28 -0500

    Added scavange function

---
+++ wilderness.h
@@ -0,0 +1,8 @@
+#include "inventory.h"
+
+#ifndef WILDERNESS_H
+#define WILDERNESS_H
+
+Item* scavange(); // This will be an arrary, the first input will tell how many items are in the arrary minus the first value
+
+#endif
