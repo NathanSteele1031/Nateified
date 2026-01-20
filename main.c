47737b056d9ea2adcd3d5930d9b2a492c299e4be
Nathan Steele <nsteele1031@gmail.com>
2026-01-20 07:58:52 -0500

    Added inventory menu function

--- main.c
+++ main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include "inventory.h"
 
-typedef struct {
-    char name[20];
-} Item;
+void inventoryMenu(Item* givenInventory, int inventoryLength){
+    printf("INVENTORY\n");
+    for (int index=0;index<inventoryLength;index++){
+        if (!itemEmpty(givenInventory[index])){
+            printf("%s: %d\n", givenInventory[index].name, givenInventory[index].amount);
+        }
+    }
+}
 
 int main() {
     Item inventory[10];
-
-
+    clearInventory(inventory, 10);
 
     return 0;
 }
