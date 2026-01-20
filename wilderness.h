68618c771f9ec14fb5208b77ac96377764660b70
Nathan Steele <nsteele1031@gmail.com>
2026-01-20 09:48:31 -0500

    Added random function

--- wilderness.h
+++ wilderness.h
@@ -4,5 +4,6 @@
 #define WILDERNESS_H
 
 Item* scavange(); // This will be an arrary, the first input will tell how many items are in the arrary minus the first value
+int getRandomValue(int max, int min);
 
 #endif
