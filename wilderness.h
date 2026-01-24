727140417f7d34dbe99f52e39ee1f0f9c7fb85ec
Nathan Steele <nsteele1031@gmail.com>
2026-01-24 10:22:50 -0500

    Added random int generator

--- wilderness.h
+++ wilderness.h
@@ -1,9 +1,11 @@
 #include "inventory.h"
+#include <stdlib.h>
+#include <time.h>
 
 #ifndef WILDERNESS_H
 #define WILDERNESS_H
 
 Item* scavange(); // This will be an arrary, the first input will tell how many items are in the arrary minus the first value
-int getRandomValue(int max, int min);
+int getRandomValue(int min, int max);
 
 #endif
