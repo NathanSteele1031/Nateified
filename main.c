cb7af44ac25a518a25d77043a003c4ee933921bd
Nathan Steele <nsteele1031@gmail.com>
2026-01-26 15:17:08 -0500

    Added the ability to switch menus.

--- main.c
+++ main.c
@@ -20,13 +20,28 @@ int main() {
     Item inventory[10];
     clearInventory(inventory, 10);
     char tempInput = '0';
+    int menuSelection = 0;
+    int mainMenuOptions = 2;
 
     while (1){
-        inventoryMenu(&inventory[0], 10);
+        switch (menuSelection){
+            case 0:
+                inventoryMenu(&inventory[0], 10);
+                break;
+            case 1:
+                wildernessMenu();
+                break;
+        }
+        printf("\nPress q to exit and 0 to switch menus\n");
         scanf("%c", &tempInput);
         if (tempInput == 'q'){
             break;
         }
+
+        if (tempInput == '0'){
+            menuSelection = (menuSelection + 1) % mainMenuOptions;
+        }
+
         if (tempInput != '\n'){
             scanf("%c", &tempInput);
         }
